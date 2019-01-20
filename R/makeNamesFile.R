e47fd2b2af9748d8c7cef8b04200a339b47f2dae
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-01-20 11:47:12 +0100

    Fix umlaut parsing and semicolon escaping

--- R/makeNamesFile.R
+++ R/makeNamesFile.R
@@ -96,9 +96,6 @@ makeNamesFile <-
     out
   }
 
-escapes <- function(x, chars = c(":", ";", "|")) {
-  for (i in chars)
-    x <- gsub(i, paste("\\", i, sep = ""), x, fixed = TRUE)
-  gsub("([^[:alnum:]^[:space:]])", "\\\\\\1", x, useBytes = TRUE)  
+escapes <- function(x, pattern = "([,:|])") {
+  gsub(pattern,  "\\\\\\1", x, useBytes = TRUE)
 }
-
